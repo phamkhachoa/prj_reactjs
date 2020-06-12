e019e8aeb77a7736f6b265a469a994b55776331a
vietlv <levanviet1774691708@gmail.com>
2020-06-12 17:26:43 +0700

    c

---
+++ 027_react_router/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <div class="container">
+                    <p>&copy; Your Website 2019. All Rights Reserved.</p>
+                    <ul class="list-inline">
+                        <li class="list-inline-item">
+                            <a href="#">Privacy</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">Terms</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">FAQ</a>
+                        </li>
+                    </ul>
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
